6f0c293ab4c908da6b81302f00fbc24405d201a9
irarubrian <brian.o.iraru@gmail.com>
2025-02-12 21:14:12 +0300

    commit

--- lib/testing/subdirectory/bool_test.py
+++ lib/testing/subdirectory/bool_test.py
@@ -3,5 +3,6 @@
 from bool_functions import return_true
 
 def test_return_true():
-    '''in bool_functions, function "return_true" returns True.'''
-    assert return_true() == True
+    '''In bool_functions, function "return_true" returns True.'''
+    print("Function output:", return_true())  # Prints the actual return value
+    assert return_true() == False  # Assertion for pytest
